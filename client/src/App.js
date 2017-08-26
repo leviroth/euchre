305458dbe2b6aceb5accb6eccef5ad1df5f9d131
Levi Roth <levimroth@gmail.com>
2017-08-26 18:26:42 -0400

    Move connection layer to its own file

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import autobahn from "autobahn";
 
 import "./App.css";
 import ChatBox from "./chat.js";
+import GameAPIConnection from "./connection-layer.js";
 import { suitToSymbol } from "./helpers.js";
 import { Bid1Controls, Bid2Controls } from "./movecontrols.js";
 import UIButton from "./uibutton.js";
@@ -18,89 +19,6 @@ function resolvePlayerPosition(position, player) {
       return "right";
     default:
       return "bottom";
-  }
-}
-
-class GameAPIConnection {
-  constructor(session, playerID) {
-    this.session = session;
-    this.playerID = playerID;
-  }
-
-  callAPI(endpoint, args) {
-    return this.session.call(`player${this.playerID}.${endpoint}`, args);
-  }
-
-  bid1(call, alone) {
-    if (call) {
-      this.performMove("call_one", alone);
-    } else {
-      this.performMove("pass_bid");
-    }
-  }
-
-  bid2(call, alone, suit) {
-    if (call) {
-      this.performMove("call_two", alone, suit);
-    } else {
-      this.performMove("pass_bid");
-    }
-  }
-
-  joinSeat(position) {
-    return this.callAPI("join_seat", [position]);
-  }
-
-  performMove(...args) {
-    this.callAPI("perform_move", args);
-  }
-
-  sendMessage(message) {
-    this.callAPI("chat", [message]);
-    /* this.addMessage(messageObj);*/
-  }
-
-  setName(name) {
-    this.callAPI("set_name", [name]);
-  }
-
-  startGame() {
-    this.callAPI("start_game");
-  }
-
-  subscribe(endpoint, callback) {
-    this.session.subscribe(endpoint, callback);
-  }
-
-  subscribeToHand(callback) {
-    this.subscribe(`hands.player${this.playerID}`, callback);
-  }
-
-  subscribeToChat(callback) {
-    this.subscribe("chat", callback);
-  }
-
-  subscribeToPublicState(callback) {
-    this.subscribe("publicstate", ([res]) => callback(this.translateStateDict(res)));
-  }
-
-  subscribeToSeats(callback) {
-    this.subscribe("seats", callback);
-  }
-
-  translateStateDict(state) {
-    return {
-      dealer: state.dealer,
-      hands: state.hands,
-      phase: state.phase,
-      score: state.score,
-      sitting: state.sitting,
-      trick: state.trick,
-      trickScore: state.trick_score,
-      trump: state.trump,
-      turn: state.turn,
-      upcard: state.up_card
-    };
   }
 }
 
